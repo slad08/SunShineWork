7231e27db10410bd0daa6669549055a58ad3e2bf
slad08 <slad_den@mail.ru>
2016-04-14 22:59:07 +0300

    Detal activity

--- app/src/main/java/android/bignerdranch/com/myapplimenu/DetailActivity.java
+++ app/src/main/java/android/bignerdranch/com/myapplimenu/DetailActivity.java
@@ -1,7 +1,9 @@
 package android.bignerdranch.com.myapplimenu;
 
+import android.app.Activity;
+
 /**
  * Created by Denis on 11.04.2016.
  */
-public class DetailActivity {
+public class DetailActivity extends Activity {
 }
