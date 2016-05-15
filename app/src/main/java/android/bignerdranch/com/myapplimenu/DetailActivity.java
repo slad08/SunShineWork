594995fe849d8f904cacbf73c3cc1efa41b4dfd4
slad08 <slad_den@mail.ru>
2016-05-15 21:45:24 +0300

    3.Second Activity Detail

--- app/src/main/java/android/bignerdranch/com/myapplimenu/DetailActivity.java
+++ app/src/main/java/android/bignerdranch/com/myapplimenu/DetailActivity.java
@@ -1,9 +1,54 @@
 package android.bignerdranch.com.myapplimenu;
 
-import android.app.Activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Denis on 11.04.2016.
  */
-public class DetailActivity extends Activity {
+public class DetailActivity extends ActionBarActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState ==null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_detal,new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.detail,menu);
+        return true;
+    }
+    @Override
+        public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id ==R.id.action_setting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+}
+    public static class PlaceholderFragment extends Fragment {
+        public  PlaceholderFragment(){
+
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState){
+            View rootView = inflater.inflate(R.layout.fragment_deatil,container,false);
+            return rootView;
+        }
+    }
 }
